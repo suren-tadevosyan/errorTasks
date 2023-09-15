2a31c962ba9594e9224b5cd088fd5ef3c002eba0
suren <tadevosyansuren07@gmail.com>
2023-09-15 13:55:18 +0400

    converting cmmented code with try/catch method

--- convert.js
+++ convert.js
@@ -1,3 +1,10 @@
+// function reverseString(s) {
+//     typeof s !== "string"
+//     ? console.log("s.split is not a function")
+//     : (s = s.split("").reverse().join(""));
+//     console.log(s);
+//     }
+
 function reverseString(str) {
   try {
     str = str.split("").reverse().join("");
